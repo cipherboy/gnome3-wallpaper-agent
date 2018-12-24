c8f249fbe28b13129a7a09cce9c16b3a01ae47ec
Alexander Scheel <alexander.m.scheel@gmail.com>
2018-12-23 20:25:34 -0700

    Clean up wrapper script
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- wrapper.sh
+++ wrapper.sh
@@ -2,13 +2,15 @@
 
 script_dir="$(dirname "${BASH_SOURCE[@]}")"
 config_dir="$HOME/.config/gnome3-wallpaper-agent"
+pid_file="$config_dir/gnome3-wallpaper-agent.pid"
+log_file="$config_dir/gnome3-wallpaper-agent.log"
 
 mkdir -p "$config_dir"
 
-echo "$BASHPID" > "$config_dir/gnome3-wallpaper-agent.pid"
+echo "$BASHPID" > "$pid_file"
 
-while [ 1 ]; do
-    python3 "$script_dir/wallpaper_agent.py" 2>&1 > "$config_dir/gnome3-wallpaper-agent.log" 2>&1
+while true; do
+    python3 "$script_dir/wallpaper_agent.py" 2>&1 >> "$log_file" 2>&1
 
     # Ensure some time between calls so that we don't lock the system when
     # wallpaper_agent.py keeps crashing.
